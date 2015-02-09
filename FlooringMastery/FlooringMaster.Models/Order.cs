2fa31cf96f4e395bcf1a191fab8279bcd053e665
KristinaSigler <kesigler@gmail.com>
2015-02-09 11:44:53 -0600

    Initial Setup of FlooringMastery Project

---
+++ FlooringMastery/FlooringMaster.Models/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlooringMastery.Models
+{
+    public class Order
+    {
+        public string OrderNumber { get; set; }
+        public string CustomerName { get; set; }
+        public Product OrderProduct { get; set; }
+        public State OrderState { get; set; }
+        public decimal Area { get; set; }
+        public decimal TotalLaborCost { get; set; }
+        public decimal TotalMaterialCost { get; set; }
+        public decimal TotalTax { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}
