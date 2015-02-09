241bcb7fe1d0c90c83ab0c19dbe283e62e15657f
andrewking0207 <andrewking0207@gmail.com>
2015-02-09 15:50:23 -0600

    not feeling very good about this

---
+++ FlooringMastery/FlooringMastery.BLL/WorkingMemory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlooringMastery.Models;
+
+namespace FlooringMastery.BLL
+{
+    public static class WorkingMemory
+    {
+        public static List<State> StateList = new List<State>(); 
+    }
+}
