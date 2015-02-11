15011f03eaebf565cbb4491afc4e77a06e0e114d
KristinaSigler <kesigler@gmail.com>
2015-02-11 09:19:07 -0600

    Commit w/out erroneous files

---
+++ FlooringMastery/FlooringMastery.BLL/AddOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlooringMastery.Models;
+
+namespace FlooringMastery.BLL
+{
+    public static class AddOrder
+    {
+        public static void AddOrderToList(Order myOrder)
+        {
+            WorkingMemory.OrderList.Add(myOrder);
+        }
+    }
+}
