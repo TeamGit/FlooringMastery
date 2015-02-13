6c1d51d33ece7c821cdb3bcc094e7e9da0388c1a
andrewking0207 <andrewking0207@gmail.com>
2015-02-13 10:54:35 -0600

    CHanged got state error prompt to only display when error

--- FlooringMastery/FlooringMastery.UI/Input.cs
+++ FlooringMastery/FlooringMastery.UI/Input.cs
@@ -184,8 +184,12 @@ namespace FlooringMastery.UI
                 {
                     gotState = true;
                 }
-                Console.WriteLine("That isn't recognized as a state in which SWC currently operates.  \nPlease try again.");
-                Console.WriteLine("SWC currently operates in the following states: ");
+                else
+                {
+                    Console.WriteLine("That isn't recognized as a state in which SWC currently operates.  \nPlease try again.");
+                    Console.WriteLine("SWC currently operates in the following states: ");
+                }
+
                 foreach (var state in WorkingMemory.StateList)
                 {
                     Console.WriteLine(state.StateAbbreviation);
