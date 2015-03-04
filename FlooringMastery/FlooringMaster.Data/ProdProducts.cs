41e627fc9987dd5cf6b6c9642172e3debb0775ef
MattFischard <fishmatt33@gmail.com>
2015-03-04 15:54:16 -0600

    Adjusted the ProdProducts and ProdStates classes to appropriately read the txt files.  These still contain the workingMemory class because I wasn't sure if this was still needed or not.

--- FlooringMastery/FlooringMaster.Data/ProdProducts.cs
+++ FlooringMastery/FlooringMaster.Data/ProdProducts.cs
@@ -11,11 +11,60 @@ namespace FlooringMaster.Data
 {
     public class ProdProducts : IContainProducts
     {
-        public List<Product> GetProducts()
+        /// <summary>
+        /// Read the product specification file, store the list in working memory.
+        /// </summary>
+        /// <returns></returns>
+        List<Product> IContainProducts.GetProducts()
         {
-            List<Product> products = new List<Product>();
+            //Are we creatign a new class in the BLL for this or will the new list product be located in one of the 
+            //existing classes in the BLL?  Or is this not needed anymore?  Same question at the bottom of this class
+            //WorkingMemory.ProductList.Clear();
+            using (StreamReader sr = new StreamReader(@"..\..\..\Documents\Products.txt"))
+                while (!sr.EndOfStream)
+                {
+                    string WholeProduct = sr.ReadLine();
+                    if (!string.IsNullOrEmpty(WholeProduct))
+                    {
+                        string[] WholeProductArray = WholeProduct.Split(',');
 
-            return (products);
+                        if (WholeProductArray[0] == "ProductType")
+                        {
+                            WholeProduct = sr.ReadLine();
+                            if (!string.IsNullOrEmpty(WholeProduct))
+                            {
+                                WholeProductArray = WholeProduct.Split(',');
+                            }
+
+                        }
+
+                        //Is this moving to calculation in the BLL?
+
+                        Product newProduct = new Product();
+
+                        newProduct.ProductType = WholeProductArray[0];
+
+                        Decimal costPerSquareFoot;
+                        if (Decimal.TryParse(WholeProductArray[1], out costPerSquareFoot))
+                        {
+                            newProduct.CostPerSquareFoot = costPerSquareFoot;
+                        }
+
+                        Decimal laborCostPerSquareFoot;
+                        if (Decimal.TryParse(WholeProductArray[2], out laborCostPerSquareFoot))
+                        {
+                            newProduct.LaborCostPerSquareFoot = laborCostPerSquareFoot;
+                        }
+
+                        //Are we creatign a new class in the BLL for this or will the new list product be located in one of the existing classes in the BLL? Or is this not needed anymore?
+                        //WorkingMemory.ProductList.Add(newProduct);
+
+                    }
+                }
+            return new List<Product>();
+
         }
+        
+
     }
 }
--- FlooringMastery/FlooringMaster.Data/ProdStates.cs
+++ FlooringMastery/FlooringMaster.Data/ProdStates.cs
@@ -15,12 +15,55 @@ namespace FlooringMaster.Data
         /// <summary>
         /// Read a file containing state tax specifications, read each line into a state object and store it in a list
         /// </summary>
-        public List<State> GetStates()
+        /// <returns></returns>
+        List<State> IContainStates.GetStates()
         {
-            List<State> states = new List<State>();
+            //Are we creatign a new class in the BLL for this or will the new list product be located in one of the 
+            //existing classes in the BLL?  Or is this not needed anymore?  Same question at the bottom of this class
+            //WorkingMemory.StateList.Clear();
 
-            return (states);
+            using (StreamReader sr = new StreamReader(@"..\..\..\Documents\Taxes.txt"))
+                while (!sr.EndOfStream)
+                {
+                    string WholeState = sr.ReadLine();
+                    if (!string.IsNullOrEmpty(WholeState))
+                    {
+                        string[] WholeStateArray = WholeState.Split('|');
+
+                        State newState = new State();
+
+                        if (WholeStateArray[0] == "StateAbbreviation")
+                        {
+                            WholeState = sr.ReadLine();
+                            if (!string.IsNullOrEmpty(WholeState))
+                            {
+                                WholeStateArray = WholeState.Split('|');
+                            }
+                        }
+
+                        if (WholeStateArray[1] == "StateName")
+                        {
+                            WholeState = sr.ReadLine();
+                            if (!string.IsNullOrEmpty(WholeState))
+                            {
+                                WholeStateArray = WholeState.Split('|');
+                            }
+                        }
+
+                        Decimal taxRate;
+                        if (Decimal.TryParse(WholeStateArray[2], out taxRate))
+                        {
+                            newState.TaxRate = taxRate/100;
+                        }
+
+                        //Are we creatign a new class in the BLL for this or will the new list product be located in one of the existing classes in the BLL? Or is this not needed anymore?
+                        //WorkingMemory.StateList.Add(newState);
+                    }
+                }
+            return new List<State>();
         }
+
+
     }
 }
     
