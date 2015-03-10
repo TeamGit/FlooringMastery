914561777e9c6e46478bca99ca39b6f766933750
MattFischard <fishmatt33@gmail.com>
2015-03-10 16:29:09 -0500

    Added validation class with methods

---
+++ FlooringMastery/FlooringMastery.BLL/Validation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using FlooringMastery.Models;
+
+
+namespace FlooringMastery.BLL
+{
+    public static class Validation
+    {
+        private static State GetState()
+        {
+            
+            string stateAbbrev = new string(State.StateAbbreviation);
+            
+            if (((((stateAbbrev == "MN") || "IA") || "WI") || "ND") || "SD")
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("That isn't recognized as a state in which SWC currently operates.  \nPlease try again.");
+                return false;
+            }
+         
+        }
+
+        private static Product GetProduct()
+        {
+            
+        }
+
+        private static Order GetOrder()
+        {
+            
+        }
+    }
+}
