68c322c7d61d4a68f75fc1144399d661643d46f5
MattFischard <fishmatt33@gmail.com>
2015-03-11 14:24:39 -0500

    Finished the validation methods within the validation class

--- FlooringMastery/FlooringMastery.BLL/Validation.cs
+++ FlooringMastery/FlooringMastery.BLL/Validation.cs
@@ -8,6 +8,7 @@
 //using FlooringMastery.Models;
 
 
+<<<<<<< 706613dd45ad2e447b76b94a5adb2916067b889e
 //namespace FlooringMastery.BLL
 //{
 //    public static class Validation
@@ -36,6 +37,45 @@
 
 //        private static Order GetOrder()
 //        {
+=======
+namespace FlooringMastery.BLL
+{
+    public static class Validation
+    {
+        private static string GetState(string stateAbbrev)
+        {
+            if ((stateAbbrev == "MN") || (stateAbbrev == "IA") || (stateAbbrev == "WI") || (stateAbbrev == "ND") || (stateAbbrev == "SD"))
+            {
+                return null;
+            }
+            else
+            {
+                return ("That isn't recognized as a state in which SWC currently operates.  \nPlease try again.");
+            }
+        }
+
+        private static string GetProduct(string productOrdered)
+        {
+            if ((productOrdered == "Laminate") || (productOrdered == "Carpet") || (productOrdered == "Wood") || (productOrdered == "Tile"))
+            {
+                return null;
+            }
+            else
+            {
+                return ("That is not a product currently carried by SWC.  \nPlease try again.");
+            }
+        }
+
+        private static List<String> GetOrder(Order myOrder)
+        {
+            var resultProd = GetProduct(myOrder.ProductType);
+            List<String> errorProdMessages = new List<string>();
+            errorProdMessages.Add(resultProd);
+
+            var resultState = GetState(myOrder.StateAbbreviation);
+            List<String> errorStateMessages = new List<string>();
+            errorStateMessages.Add(resultState);
+>>>>>>> 28e216b0ac756b9ce2bdf5d1bc9f70765cad6ac1
             
 //        }
 //    }
